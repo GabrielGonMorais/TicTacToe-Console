12ee2f9517c16d3071745ad3cf286cb91c9d9ab0
GabrielMorais <morais.fy@msn.com>
2021-11-10 15:30:48 -0300

    cleaner

--- Jogodavelha/Program.cs
+++ Jogodavelha/Program.cs
@@ -4,17 +4,17 @@ namespace Jogodavelha
 {
     class Program
     {
-        public static Players player1 = new Player1();
-        public static Players player2 = new Player2();        
-
+        public static Players player1 = new Players() { Symbol = 'X' };
+        public static Players player2 = new Players() { Symbol = 'O' };
+                  
         static void Main()
         {          
             Console.WriteLine("## Welcome to the Tic-Tac-Toe ##\n\n");
             Functionalities.FixedGameBoard();
             
-            player1.ChooseTheName();
+            player1.ChooseTheName("first");            
 
-            player2.ChooseTheName();
+            player2.ChooseTheName("second");
 
             Console.Clear();
             Functionalities.GameLoop();
